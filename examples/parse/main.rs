285ed41d67cd27e665072a51210124fa0fd9d137
mrpedrobraga <mrhenribraga@gmail.com>
2024-06-15 14:07:40 -0300

    feat: added checking to example, although i still don't get it

--- examples/parse/main.rs
+++ examples/parse/main.rs
@@ -1,22 +1,28 @@
+use bend::diagnostics::DiagnosticsConfig;
 use bend::fun::load_book::do_parse_book;
+use bend::{check_book, CompileOpts};
 use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-    let source = "def main():\n  return \"Hello, World!\"";
+    let path_buf = PathBuf::from("./examples/hello_world.bend");
+    let path = path_buf.as_path();
+    let source = std::fs::read_to_string(path).unwrap();
 
-    let book_res = do_parse_book(
-        source,
-        PathBuf::from("./example.bend").as_path(),
-        bend::fun::Book::builtins(),
-    );
+    let book_res = do_parse_book(source.as_str(), path, bend::fun::Book::builtins());
 
     match book_res {
-        Ok(v) => {
-            fs::write("./examples/parse/book.txt", format!("{:#?}", v)).unwrap();
+        Ok(mut book) => {
+            let book_check = check_book(
+                &mut book,
+                DiagnosticsConfig::default(),
+                CompileOpts::default(),
+            );
+            print!("{:#?}", book_check);
+            fs::write("./examples/parse/book.txt", format!("{:#?}", book)).unwrap();
         }
         Err(e) => {
-            eprintln!("Error: {:#?}", e);
+            print!("{}", e);
         }
     }
 }
