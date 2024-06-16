52a07f1f5a699e028e09c29a8bd4043b9010a9a2
mrpedrobraga <mrhenribraga@gmail.com>
2024-06-15 21:38:14 -0300

    feat: Added WASI target -- but did not implement it yet.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use bend_language_server::backend::Backend;
 use tower_lsp::{LspService, Server};
+pub mod backend;
+pub mod introspection;
 
-#[tokio::main]
+use backend::Backend;
+
+#[cfg(target_arch = "wasm32")]
+#[tokio::main(flavor = "current_thread")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub async fn main() {
+    compile_error!("Needs implementing for WASI.")
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
     let (service, socket) = LspService::new(|client| Backend::new(client));
